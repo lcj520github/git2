33f839ffdbfe2a1b66b14391c7579ad3b76cef2d
lcj520github <104203048+lcj520github@users.noreply.github.com>
2022-08-17 16:47:38 +0800

    修改代码

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,5 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("你好demo1");
+        System.out.println("你好demo2");
+        System.out.println("你好demo3");
     }
 }
