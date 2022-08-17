5add582fda8447dc53861602f2bf1d57ec4e985c
lcj520github <104203048+lcj520github@users.noreply.github.com>
2022-08-17 17:18:17 +0800

    再次完善你的代码

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -3,5 +3,7 @@ public class demo {
         System.out.println("你好demo1");
         System.out.println("你好demo2");
         System.out.println("你好demo3");
+        System.out.println("你好demo4");
+        System.out.println("你好demo5");
     }
 }
